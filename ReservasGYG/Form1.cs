d14d906014bd2137c6711d3845cd9fce4415efe0
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2023-09-18 08:09:18 +0200

    v1.0.56

--- ReservasGYG/Form1.cs
+++ ReservasGYG/Form1.cs
@@ -26,18 +26,18 @@ public partial class Form1 : Form
 
     // Intentar no pasar de estas marcas: 60 caracteres. 2         3         4         5         6
     //                                ---------|---------|---------|---------|---------|---------|
-    //[COPIAR]AppDescripcionCopia = " colores y textos botones app móvil"
+    //[COPIAR]AppDescripcionCopia = " asocio app UWP a la MS store"
     // BuscarClientes mostrar reservas en la pagina
 
     /// <summary>
     /// La versión de la aplicación.
     /// </summary>
-    public static string AppVersion { get; } = "1.0.55";
+    public static string AppVersion { get; } = "1.0.56";
 
     /// <summary>
     /// La versión del fichero (la revisión)
     /// </summary>
-    public static string AppFileVersion { get; } = "1.0.55.0";
+    public static string AppFileVersion { get; } = "1.0.56.0";
 
     /// <summary>
     /// La fecha de última actualización
