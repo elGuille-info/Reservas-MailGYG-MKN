06675cb6cd33f2287456f187329f34c7bd20518e
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2023-10-02 12:13:32 +0200

    v1.0.96 textos

--- ReservasGYG/Readme.md
+++ ReservasGYG/Readme.md
@@ -1,3 +1,3 @@
 ﻿# Reservas Mail GYG
 
-Aplicación para gestionar las reservas recibidas por email de GetYourGuide con MKN Reservas  (revisión del 27-sep-2023)
+Aplicación para gestionar las reservas recibidas por email de GetYourGuide con MKN Reservas  (revisión del 02-oct-2023)
--- ReservasGYG_Movil/ReservasGYG_Movil/App.xaml.cs
+++ ReservasGYG_Movil/ReservasGYG_Movil/App.xaml.cs
@@ -26,22 +26,22 @@ namespace ReservasGYG_Movil
 
         // Intentar no pasar de estas marcas: 60 caracteres. 2         3         4         5         6
         //                                ---------|---------|---------|---------|---------|---------|
-        //[COPIAR]AppDescripcionCopia = " versión en app móvil y cambio linea iOS"
+        //[COPIAR]AppDescripcionCopia = " textos"
 
         /// <summary>
         /// La versión de la aplicación.
         /// </summary>
-        public static string AppVersion { get; } = "1.0.95";
+        public static string AppVersion { get; } = "1.0.96";
 
         /// <summary>
         /// La versión del fichero (la revisión)
         /// </summary>
-        public static string AppFileVersion { get; } = "1.0.95.0";
+        public static string AppFileVersion { get; } = "1.0.96.0";
 
         /// <summary>
         /// La fecha de última actualización
         /// </summary>
-        public static string AppFechaVersion { get; } = "27-sep-2023";
+        public static string AppFechaVersion { get; } = "02-oct-2023";
 
         /// <summary>
         /// El nombre de la aplicación.
