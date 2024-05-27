68e0bfa4466f6ce88955425a5784100a710c9bd2
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2024-05-27 19:21:06 +0200

    17-may-24 1921

--- ReservasGYG/Form1.cs
+++ ReservasGYG/Form1.cs
@@ -27,18 +27,18 @@ public partial class Form1 : Form
 
     // Intentar no pasar de estas marcas: 60 caracteres. 2         3         4         5         6
     //                                ---------|---------|---------|---------|---------|---------|
-    //[COPIAR]AppDescripcionCopia = " mostrar texto al cancelar sin enviar mail"
+    //[COPIAR]AppDescripcionCopia = " textos importante"
     // BuscarClientes mostrar reservas en la pagina
 
     /// <summary>
     /// La versión de la aplicación.
     /// </summary>
-    public static string AppVersion { get; } = "1.0.113";
+    public static string AppVersion { get; } = "1.0.114";
 
     /// <summary>
     /// La versión del fichero (la revisión)
     /// </summary>
-    public static string AppFileVersion { get; } = "1.0.113.0";
+    public static string AppFileVersion { get; } = "1.0.114.0";
 
     /// <summary>
     /// La fecha de última actualización
