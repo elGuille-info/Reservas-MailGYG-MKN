6b64ca52d6878e570f68680988dace77568cc17f
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2024-07-25 03:24:18 +0200

    24-jul-24 21.47

--- ReservasGYG/Form1.cs
+++ ReservasGYG/Form1.cs
@@ -27,23 +27,23 @@ public partial class Form1 : Form
 
     // Intentar no pasar de estas marcas: 60 caracteres. 2         3         4         5         6
     //                                ---------|---------|---------|---------|---------|---------|
-    //[COPIAR]AppDescripcionCopia = " no duplicar email al enviar fotos"
+    //[COPIAR]AppDescripcionCopia = " cambio el texto de mañana es el dia"
     // BuscarClientes mostrar reservas en la pagina
 
     /// <summary>
     /// La versión de la aplicación.
     /// </summary>
-    public static string AppVersion { get; } = "1.0.128";
+    public static string AppVersion { get; } = "1.0.129";
 
     /// <summary>
     /// La versión del fichero (la revisión)
     /// </summary>
-    public static string AppFileVersion { get; } = "1.0.128.0";
+    public static string AppFileVersion { get; } = "1.0.129.0";
 
     /// <summary>
     /// La fecha de última actualización
     /// </summary>
-    public static string AppFechaVersion { get; } = "23-jul-2024";
+    public static string AppFechaVersion { get; } = "24-jul-2024";
 
 
     public static Form1 Current { get; set; }
